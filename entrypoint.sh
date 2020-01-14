c0ad6303ae2e237d7ad8c72b0fb075b0302533b0
N. Sanchirico <Lnk2past@gmail.com>
2020-01-14 10:56:07 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh -l
-msg=$(make 2>&1)
-echo ::set-output name=msg::$msg
+
+build_output=$( make 2>&1 )
+if [ $? -ne 1 ]
+then
+  echo ::set-output name=build_output::$build_output
+  echo ::set-output name=test_output::
+  echo ::set-output name=score::'0'
+  exit 0
+fi
+
+test_output=$( make test 2>&1 )
+if [ $? -ne 1 ]
+then
+  echo ::set-output name=build_output::$build_output
+  echo ::set-output name=test_output::$test_output
+  echo ::set-output name=score::'7.5'
+  exit 0
+fi
+
+echo ::set-output name=build_output::$build_output
+echo ::set-output name=test_output::$test_output
+echo ::set-output name=score::'15'
