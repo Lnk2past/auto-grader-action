0ac0929242f57b746ffd836c28a9ff2eee2ae86b
N. Sanchirico <Lnk2past@gmail.com>
2020-01-13 23:35:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,24 @@
 
 cd /github/workspace
 
-build_output=$( make 2>&1)
+msg=$( make 2>&1)
 if [ $? -ne 1 ]
 then
-  exit 1
+  echo "Score: 0/15" > report.txt
+  echo "Build failure:" >> report.txt
+  echo $msg >> report.txt
+  exit 3
 fi
 
-test_output=$( make test 2>&1)
+msg=$( make test 2>&1)
 if [ $? -ne 1 ]
 then
-  exit 2
+  echo "Score: 7.5/15" > report.txt
+  echo "Test failure:" >> report.txt
+  echo $msg >> report.txt
+  exit 4
 fi
+
+echo "Score: 15/15" > report.txt
 
 exit 0
