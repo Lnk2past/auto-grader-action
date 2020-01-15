cc362dc813ae21d295e2ab3b679090ab49dd80a5
N. Sanchirico <Lnk2past@gmail.com>
2020-01-14 20:51:38 -0500

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,4 @@
-import sys
+import os, sys
 from subprocess import run, PIPE, STDOUT
 from actionspytoolkit import logging
 from github import Github
@@ -24,6 +24,6 @@ logging.set_output('test_output', test_output)
 logging.set_output('score', score)
 
 gh = Github(sys.argv[1])
-repo = gh.get_repo(sys.argv[2])
+repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
 body = '## Score:\n{}/15\n## Build Output:\n{}\n## Test Output:\n```{}```'.format(score, build_output, test_output)
 repo.create_issue(title="Grade", body=body, labels=['grade'])
