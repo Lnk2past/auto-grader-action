07e9a27ab63f6e39a5a2ac76c33a28daf13306fd
N. Sanchirico <Lnk2past@gmail.com>
2020-01-14 20:37:37 -0500

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,8 @@
+import sys
 from subprocess import run, PIPE, STDOUT
 from actionspytoolkit import logging
+from github import Github
+
 score = 0.0
 build_output = ''
 test_output = ''
@@ -16,3 +19,8 @@ if proc.returncode == 0:
 logging.set_output('build_output', build_output)
 logging.set_output('test_output', test_output)
 logging.set_output('score', score)
+
+gh = Github(sys.argv[1])
+repo = gh.get_repo(sys.argv[2])
+body = '## Score:\n{}/15\n## Build Output:\n{}\n## Test Output:\n```{}```'.format(score, build_output, test_output)
+repo.create_issue(title="Grade", body=body, labels=['grade'])
