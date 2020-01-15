ada0f8d06ef69e50584b55c1318e665d1c53436b
N. Sanchirico <Lnk2past@gmail.com>
2020-01-14 22:25:35 -0500

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -12,6 +12,7 @@ points = 0
 total_points = 0
 
 for step in configuration['steps']:
+  logging.warning(f'Running Step {step["name"]}')
   command = step['command'].split()
   total_points += step['points']
   proc = run(command, stdout=PIPE, stderr=STDOUT)
